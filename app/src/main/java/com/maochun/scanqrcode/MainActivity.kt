2378b7245a817ef2f9b46b5825b72e87efd2aafc
MaochunS <52019408+MaochunS@users.noreply.github.com>
2023-02-21 20:10:06 +0800

    update

--- app/src/main/java/com/maochun/scanqrcode/MainActivity.kt
+++ app/src/main/java/com/maochun/scanqrcode/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.maochun.scanqrcode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttonScanQrcode = findViewById<Button>(R.id.button_scan_qrcode)
+        buttonScanQrcode.setOnClickListener {
+            val intent = Intent(this, ScanQRCodeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/maochun/scanqrcode/ScanQRCodeActivity.kt
+++ app/src/main/java/com/maochun/scanqrcode/ScanQRCodeActivity.kt
@@ -1,29 +1,142 @@
 package com.maochun.scanqrcode
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+// This is an arbitrary number we are using to keep tab of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class ScanQRCodeActivity : AppCompatActivity() {
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+    private lateinit var previewView: PreviewView
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_qrcode)
 
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(
-                Barcode.FORMAT_QR_CODE,
-                Barcode.FORMAT_AZTEC)
+        previewView = findViewById(R.id.previewView)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startPreview()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startPreview()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startPreview(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            bindPreview()
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindPreview() {
+        var preview = Preview.Builder()
             .build()
 
-//        val scanner = BarcodeScanning.getClient(options)
-//        scanner.process(inputImage)
-//            .addOnSuccessListener { barcodes ->
-//                for (barcode in barcodes) {
-//                    // Handle received barcodes...
-//                }
-//            }
-//            .addOnFailureListener { }
+        var cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+
+        val analyzer = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        analyzer?.let {
+            it.setAnalyzer(cameraExecutor) { imageProxy ->
+                handleImage(imageProxy)
+                imageProxy.close()
+            }
+        }
+
+        try {
+            val cameraProvider = cameraProviderFuture.get()
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, analyzer)
+        } catch (e: Exception) {
+            Log.e("ScanQRCode Test APP", "cameraProvider binding failed!", e)
+        }
+    }
+
+    private fun handleImage(imageProxy: ImageProxy){
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            // Pass image to an ML Kit Vision API
+            val options = BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(
+                    Barcode.FORMAT_QR_CODE,
+                    Barcode.FORMAT_AZTEC)
+                .build()
+
+            val scanner = BarcodeScanning.getClient(options)
+            scanner.process(image).addOnSuccessListener { barcodes ->
+                for (barcode in barcodes) {
+                    // Handle received barcodes...
+                    println("Scan result: ${barcode.rawValue}")
+
+                    Toast.makeText(
+                        this,
+                        "Scan result: ${barcode.rawValue}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    cameraExecutor.shutdown()
+                    finish()
+                }
+            }
+
+        }
     }
 }
+
